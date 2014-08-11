c2e3cff46af07dc5b55168536dc8edcd513d89ca
Georg Krause <mail@georg-krause.net>
2014-08-12 01:54:28 +0200

    switched to luafcgi-syntay

--- blog.lua
+++ blog.lua
@@ -1,14 +1,18 @@
-home = function () 
-  ngx.print("Startseite<br />")
+local lf = require("luafcgid")
+
+function main(env, con) 
+  con:header("X-Powered-By", "Bluag")
+  
+  con:puts("Startseite<br />")
   for filename in io.popen("ls " .. localpath  .. "/data"):lines() do
     local title = filename:gsub('.txt', "")
 
-    ngx.print('<a href="/' .. title .. '">' .. title  .. '</a><br />') 
+    con:puts('<a href="/' .. title .. '">' .. title  .. '</a><br />') 
   end
 end
 
 article = function ()
-  ngx.print('<a href="/">Startseite</a></br>')
+  con:puts('<a href="/">Startseite</a></br>')
   local req_file = uri:gsub('/', "")
 
   succ, file = pcall(assert, io.open(localpath .. "/data/" .. req_file .. ".txt"))
@@ -16,6 +20,6 @@ article = function ()
     for line in file:lines() do ngx.print(line .. '<br />') end
     file:close()
   else
-    ngx.print("fail")
+    con:puts("fail")
   end
 end
